629be3da00b2b64b92be8b310bd29624cffafb46
aliahmed2007 <ali.3hmed2007@gmail.com>
2026-04-02 16:52:34 +0200

    create sidebar on for mobile and larager screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
-import Image from "next/image";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import AppSidebarClient from "./_AppSidebarClient";
 
 export default function Home() {
   return (
-    <h1>Hi</h1>
-  )
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Aether Jobs</span>
+          </SidebarHeader>
+
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Click here</SidebarMenuButton>
+              </SidebarMenuItem>
+              
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main>asdfasdfadf</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
---
+++ services/clerk/components/ClerkProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { ReactNode } from "react";
+
+function ClerkProvider({ children }: { children: ReactNode }) {
+  const isDarkMode = useIsDarkMode();
+  return (
+    <OriginalClerkProvider
+      appearance={isDarkMode ? { baseTheme: dark } : undefined}
+    >
+      {children}
+    </OriginalClerkProvider>
+  );
+}
+
+export default ClerkProvider;
