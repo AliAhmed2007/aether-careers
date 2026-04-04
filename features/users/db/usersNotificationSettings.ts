2d0f8218e405276a5aca78801657f20e783bc6aa
aliahmed2007 <ali.3hmed2007@gmail.com>
2026-04-04 11:33:38 +0200

    Add cache fuctionality to user and user settings features using nextjs cache components

--- features/users/db/usersNotificationSettings.ts
+++ features/users/db/usersNotificationSettings.ts
@@ -1,9 +1,11 @@
 import { db } from "@/drizzle/db";
 import { UserNotificationSettingsTable } from "@/drizzle/schema";
+import { revalidateUserNotificationSettingsCache } from "./cache/userNotificaionSettings";
 
 export async function insertUserNontificationSettings(settings: typeof UserNotificationSettingsTable.$inferInsert) {
     await db
         .insert(UserNotificationSettingsTable)
         .values(settings)
         .onConflictDoNothing()
+    revalidateUserNotificationSettingsCache(settings.userId)
 }
